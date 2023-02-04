8a813f0b409324ac7c82c64988b2a92aef3d2caa
milanpreet91 <kaur.milanpreet7@gmail.com>
2023-02-04 09:21:11 +0530

    Update test_mymodule.py

--- test_mymodule.py
+++ test_mymodule.py
@@ -4,8 +4,13 @@ from mymodule import square, double
 class TestMyModule(unittest Testcase):
   def test_square(self):
     self.assertEqual(square(2),4)
+    self.assertEqual(square(3.0),9.0)
+    self.assertEqual(square(-3),-9)
   def test_double(self):
     self.assertEqual(double(3),6)
+    self.assertEqual(double(2),4)
+    self.assertEqual(double(-3.1),-6.2)
+    self.assertEqual(double(0),0)
 
 if __name__ == "__main__":
   unittest.main()
